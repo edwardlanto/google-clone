77390aaaaa005ef98ce5bf9f34d34708e5006844
Edward Lanto <elanto@hotmail.com>
2020-09-03 16:07:36 -0700

    replaced key file with env

--- fe/src/hooks/useGoogleSearch.tsx
+++ fe/src/hooks/useGoogleSearch.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import API_KEY_DEV from "../keys";
 import axios from "axios";
+const API_KEY_DEV = process.env.REACT_APP_API_KEY;
 
 const API_KEY = process.env.NODE_ENV === 'development' ? API_KEY_DEV : process.env.API_KEY
 const CONTEXT_KEY = "a6733d3b6223d59bb";
