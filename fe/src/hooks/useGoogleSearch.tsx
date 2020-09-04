0f2ca61dc0447964fd83735c5f843ce298fa689b
Edward Lanto <elanto@hotmail.com>
2020-09-03 20:27:36 -0700

    secured api keys

--- fe/src/hooks/useGoogleSearch.tsx
+++ fe/src/hooks/useGoogleSearch.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 const API_KEY_DEV = process.env.REACT_APP_API_KEY;
 
-const API_KEY = process.env.NODE_ENV === 'development' ? API_KEY_DEV : process.env.API_KEY
+const API_KEY = process.env.NODE_ENV === 'development' ? process.env.API_KEY : API_KEY_DEV;
 const CONTEXT_KEY = "a6733d3b6223d59bb";
 const useGoogleSearch = (term: any) => {
   const [data, setData] = useState<any>(null);
--- index.js
+++ index.js
@@ -9,9 +9,9 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-const uri = mongoKey.dev;
+const uri = process.env.MONGO_KEY_DEV;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
+console.log('use', uri);
 const connection = mongoose.connection;
 connection.once('open', () => {
 	console.log('Mongoose Connection good');
